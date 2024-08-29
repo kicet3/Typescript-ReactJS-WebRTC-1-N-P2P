463bbfc31762e09fa3d12e347775642ebd336a4f
kicet3 <37786322+kicet3@users.noreply.github.com>
2024-08-29 19:48:13 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,60 +1,41 @@
-const express = require("express");
-const http = require("http");
-const socketIo = require("socket.io");
-
-const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
-
-const rooms = {}; // 방 정보를 저장할 객체
-
-io.on("connection", (socket) => {
-  console.log(`New client connected: ${socket.id}`);
-
-  socket.on("join_room", (data) => {
-    const roomName = data.room;
-    socket.join(roomName);
-    console.log(`Client ${socket.id} joined room ${roomName}`);
-
-    if (!rooms[roomName]) {
-      rooms[roomName] = [];
+const io = require("socket.io")(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"]
     }
-
-    rooms[roomName].push(socket.id);
-
-    const otherUsers = rooms[roomName].filter((id) => id !== socket.id);
-    socket.emit("all_users", otherUsers);
-
-    socket.to(roomName).emit("new_user", socket.id);
   });
-
-  socket.on("offer", (sdp) => {
-    console.log(`Sending offer from ${socket.id}`);
-    socket.broadcast.emit("getOffer", sdp);
-  });
-
-  socket.on("answer", (sdp) => {
-    console.log(`Sending answer from ${socket.id}`);
-    socket.broadcast.emit("getAnswer", sdp);
-  });
-
-  socket.on("candidate", (candidate) => {
-    console.log(`Sending candidate from ${socket.id}`);
-    socket.broadcast.emit("getCandidate", candidate);
-  });
-
-  socket.on("disconnect", () => {
-    console.log(`Client disconnected: ${socket.id}`);
-
-    for (const roomName in rooms) {
-      rooms[roomName] = rooms[roomName].filter((id) => id !== socket.id);
-      if (rooms[roomName].length === 0) {
-        delete rooms[roomName];
+  
+  io.on("connection", (socket) => {
+    console.log("New client connected", socket.id);
+  
+    socket.on("join_room", (data) => {
+      const roomName = data.room;
+      socket.join(roomName);
+      console.log(`Client with ID: ${socket.id} joined room: ${roomName}`);
+  
+      const allUsers = io.sockets.adapter.rooms.get(roomName);
+      if (allUsers) {
+        const usersArray = Array.from(allUsers);
+        socket.emit("all_users", usersArray);
       }
-    }
+    });
+  
+    socket.on("offer", (sdp) => {
+      const roomName = "1234";  // 예시로 고정된 룸 이름
+      socket.to(roomName).emit("getOffer", sdp);
+    });
+  
+    socket.on("answer", (sdp) => {
+      const roomName = "1234";
+      socket.to(roomName).emit("getAnswer", sdp);
+    });
+  
+    socket.on("candidate", (candidate) => {
+      const roomName = "1234";
+      socket.to(roomName).emit("getCandidate", candidate);
+    });
+  
+    socket.on("disconnect", () => {
+      console.log("Client disconnected", socket.id);
+    });
   });
-});
-
-server.listen(4000, () => {
-  console.log("Server is running on port 4000");
-});
