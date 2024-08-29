5a175b0662edfc8d58163219907b372e03437a5e
kicet3 <37786322+kicet3@users.noreply.github.com>
2024-08-29 18:59:56 +0900

    console.log. socket

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ let socketToRoom = {};
 const maximum = process.env.MAXIMUM || 4;
 
 io.on('connection', socket => {
-    console.log(`` + socket.id + ` connected`);
+    console.log(`` + socket + ` connected`);
     socket.on('join_room', data => {
         console.log('asdfasdf',data)
         if (users[data.room]) {
