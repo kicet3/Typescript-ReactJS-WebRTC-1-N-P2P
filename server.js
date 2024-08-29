3ffea9c864697949141099352825aa62a0d515f7
kicet3 <37786322+kicet3@users.noreply.github.com>
2024-08-29 18:51:48 +0900

    포트 변경

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ let socketio = require('socket.io');
 let io = socketio.listen(server);
 
 app.use(cors());
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 4000;
 
 let users = {};
 
